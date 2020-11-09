7527399e8d8e68d4555b384e543ff733d4394b28
Rhea <rheachhabra1011@gmail.com>
2020-11-09 22:24:27 +0530

    ViewModel added.

--- app/src/main/java/com/example/mvvm_jetpack_masterclass/Util/ViewModel.kt
+++ app/src/main/java/com/example/mvvm_jetpack_masterclass/Util/ViewModel.kt
@@ -8,8 +8,9 @@ import androidx.lifecycle.ViewModel
 //We can bind UI element with LiveData and vice versa-Two way binding
 //if we need to pass some input data to the constructor of the viewModel , we need to create a factory class for viewModel.
 //A ViewModel's onCleared() is called when the app is put into the background and the app process is killed in order to free up the system's memory.
+//To keep the mutable live data private, we keep a live data variable in its place because private variable wont be visible to the activity to observe.
 class ViewModel(initialName:String): ViewModel() {
-    private  var name=MutableLiveData<String>()  
+    private  var name=MutableLiveData<String>()
     val nameValue: LiveData<String>
     get() = name
 

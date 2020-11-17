b68dad67ceab44840bdde896c49303cf9dbcd029
Rhea <rheachhabra1011@gmail.com>
2020-11-17 17:34:08 +0530

    Suspending functions.

--- app/src/main/java/com/example/mvvm_jetpack_masterclass/CoroutineOne.kt
+++ app/src/main/java/com/example/mvvm_jetpack_masterclass/CoroutineOne.kt
@@ -3,9 +3,9 @@ package com.example.mvvm_jetpack_masterclass
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import androidx.databinding.DataBindingUtil
+import com.example.mvvm_jetpack_masterclass.databinding.ActivityCoroutineOneBinding
+import kotlinx.coroutines.*
 
 //Coroutines enable cooperative multitasking i.e processes control their own behaviour.
 //Phones have a refresh rate of more than 60Hz therefore it is important that the main thread does not do any heavy computing.
@@ -14,15 +14,34 @@ import kotlinx.coroutines.launch
 //We can control a coroutine from multiple threads irrespective of in which thread it was created.
 //We can write synchronous code for a coroutine thus reducing boilerplate code.
 class CoroutineOne : AppCompatActivity() {
+    lateinit var binding:ActivityCoroutineOneBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_coroutine_one)
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_coroutine_one)
+        binding.start.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+               download()
+            }
+        }
 
         CoroutineScope(Dispatchers.IO).launch {
             Log.i("Message1","Hello from ${Thread.currentThread().name}")
         }
         CoroutineScope(Dispatchers.Main).launch {
             Log.i("Message1","Hello from ${Thread.currentThread().name}")
+        }
+    }
+
+    //switching to main thread to change UI
+    private suspend fun download()
+    {
+        withContext(Dispatchers.Main)
+        {
+            for(i in 1..20000)
+                {
+                    binding.textview.setText("i= ${i} from ${Thread.currentThread().name}")
+                    delay(1000)
+                }
         }
     }
 }
